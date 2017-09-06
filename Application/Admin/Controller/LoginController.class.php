622e6d3ea6ee3b1529ad6861cc5b7fdef4599944
skyloong <skyloongltl@163.com>
2017-09-06 15:41:03 +0800

    --

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -1,47 +1,49 @@
 <?php
+
 namespace Admin\Controller;
+
 use Common\Controller\PubController;
-class LoginController extends PubController{
-    public function login(){
-        if(IS_AJAX){
-            $post = I('post.');
-            $model = D('user');
-            if(check_verify($post['verify'])){
-                $where['username'] = $post['username'];
-                $data = $model->where($where)->find();
-                if($data == false || $data == null){
-                    $ret = retMessage('', array($model->getDbError()));
-                    $this->ajaxReturn($ret, 'JSON');
-                }
-                if($data['is_admin'] == '2' || $data['is_admin'] == '3'){
-                    if($data['passwd'] == md5($post['passwd'].$data['salt'])){
-                        $insert['last_login_time'] = time();
-                        $insert['last_login_ip'] = get_client_ip();
-                        $insert['login_times'] = (int)$data['login_times']++;
-                        if(!$model->where($where)->save($insert)){
-                            $ret = retErrorMessage('登录失败');
-                            $this->ajaxReturn($ret, 'JSON');
-                        }
-                        session(array('name'=>'admin_user','expire'=>60*60*3));
-                        session('admin_user', 'is_admin');
-                        session('admin_username', $post['username']);
-                        $ret = retMessage('登录成功');
-                    }else{
-                        $ret = retErrorMessage('密码或帐号错误');
-                    }
-                }else{
-                    $ret = retErrorMessage('不是管理员');
-                }
-            }else{
-                $ret = retErrorMessage('验证码错误');
+
+class LoginController extends PubController
+{
+    public function login()
+    {
+        $post = I('post.');
+        $model = D('user');
+        if (check_verify($post['verify'])) {
+            $where['username'] = $post['username'];
+            $data = $model->where($where)->find();
+            if ($data == false || $data == null) {
+                $ret = retMessage('', array($model->getDbError()));
+                $this->ajaxReturn($ret, 'JSON');
             }
-            $this->ajaxReturn($ret, 'JSON');
-        }else{
-            $this->display();
+            if ($data['is_admin'] == '2' || $data['is_admin'] == '3') {
+                if ($data['passwd'] == md5($post['passwd'] . $data['salt'])) {
+                    $insert['last_login_time'] = time();
+                    $insert['last_login_ip'] = get_client_ip();
+                    $insert['login_times'] = (int)$data['login_times']++;
+                    if (!$model->where($where)->save($insert)) {
+                        $ret = retErrorMessage('登录失败');
+                        $this->ajaxReturn($ret, 'JSON');
+                    }
+                    session(array('name' => 'admin_user', 'expire' => 60 * 60 * 3));
+                    session('admin_user', 'is_admin');
+                    session('admin_username', $post['username']);
+                    $ret = retMessage('登录成功');
+                } else {
+                    $ret = retErrorMessage('密码或帐号错误');
+                }
+            } else {
+                $ret = retErrorMessage('不是管理员');
+            }
+        } else {
+            $ret = retErrorMessage('验证码错误');
         }
+        $this->ajaxReturn($ret, 'JSON');
     }
 
-    public function showVerify(){
+    public function showVerify()
+    {
         show_verify();
     }
 }
--- Application/Home/Controller/SignController.class.php
+++ Application/Home/Controller/SignController.class.php
@@ -1,55 +1,57 @@
 <?php
+
 namespace Home\Controller;
+
 use Think\Controller;
-class SignController extends Controller{
+
+class SignController extends Controller
+{
     public function sign()
     {
-        if (IS_AJAX) {
-            $model = M('User');
-            $post = I('post.');
-            $where['username'] = $post['username'];
-            if (empty($post['username'])) {
-                $ret = retErrorMessage('用户名不能为空');
-                $this->ajaxReturn($ret, 'JSON');
-            }elseif (empty($post['passwd'])) {
-                $ret = retErrorMessage('密码为空');
-                $this->ajaxReturn($ret, 'JSON');
-            }elseif (empty($post['email'])) {
-                $ret = retErrorMessage('邮箱不能为空');
-                $this->ajaxReturn($ret, 'JSON');
-            }
-            $user = $model->where($where)->find();
-            $wh['email'] = $post['email'];
-            $email = $model->where($wh)->find();
-            if(!empty($email)){
-                $ret = retErrorMessage('邮箱已被注册');
-                $this->ajaxReturn($ret, 'JSON');
-                exit;
-            }
-            if (!empty($user)) {
-                $ret = retErrorMessage('帐号已被注册');
-                $this->ajaxReturn($ret, 'JSON');
-                exit;
-            }
-            $data['username'] = $post['username'];
-            $data['create_time'] = time();
-            $data['email'] = $post['email'];
-            $data['is_admin'] = 0;
-            $data['salt'] = md5(time() . mt_rand(1, 1000));
-            $data['passwd'] = md5($post['passwd'] . $data['salt']);
-
-            if ($model->add($data) !== false) {
-                $ret = retMessage('注册成功');
-            } else {
-                $ret = retErrorMessage('注册失败');
-            }
-        }else{
-            $ret = retErrorMessage('请通过正常渠道注册');
+        $model = M('User');
+        $post = I('post.');
+        $where['username'] = $post['username'];
+        if (empty($post['username'])) {
+            $ret = retErrorMessage('用户名不能为空');
+            $this->ajaxReturn($ret, 'JSON');
+        } elseif (empty($post['passwd'])) {
+            $ret = retErrorMessage('密码为空');
+            $this->ajaxReturn($ret, 'JSON');
+        } elseif (empty($post['email'])) {
+            $ret = retErrorMessage('邮箱不能为空');
+            $this->ajaxReturn($ret, 'JSON');
         }
+        $user = $model->where($where)->find();
+        $wh['email'] = $post['email'];
+        $email = $model->where($wh)->find();
+        if (!empty($email)) {
+            $ret = retErrorMessage('邮箱已被注册');
+            $this->ajaxReturn($ret, 'JSON');
+            exit;
+        }
+        if (!empty($user)) {
+            $ret = retErrorMessage('帐号已被注册');
+            $this->ajaxReturn($ret, 'JSON');
+            exit;
+        }
+        $data['username'] = $post['username'];
+        $data['create_time'] = time();
+        $data['email'] = $post['email'];
+        $data['is_admin'] = 0;
+        $data['salt'] = md5(time() . mt_rand(1, 1000));
+        $data['passwd'] = md5($post['passwd'] . $data['salt']);
+
+        if ($model->add($data) !== false) {
+            $ret = retMessage('注册成功');
+        } else {
+            $ret = retErrorMessage('注册失败');
+        }
+
         $this->ajaxReturn($ret, 'JSON');
     }
 
-    public function showVerify(){
+    public function showVerify()
+    {
         show_verify();
     }
 }
