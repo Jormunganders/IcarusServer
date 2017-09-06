6522721a01e25c152ca04a757c707d1eacd5d18a
skyloong <skyloongltl@163.com>
2017-09-06 15:06:02 +0800

    在index.php加入header

--- Application/Common/Controller/BaseController.class.php
+++ Application/Common/Controller/BaseController.class.php
@@ -3,6 +3,6 @@ namespace Common\Controller;
 use Think\Controller;
 class BaseController extends Controller{
     public function _initialize(){
-        header("Access-Control-Allow-Origin: *");
+
     }
 }
